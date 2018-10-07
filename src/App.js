ac1e556e1cdbb12055217c2d962efb0ef7ba9b1b
akshay-damodaran <akshay.damodaran2411@gmail.com>
2018-10-07 17:11:53 +0530

    sending key as props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react'
 import { Switch, Route, Redirect } from "react-router-dom";
+
+import Auth from './Containers/Auth';
+import Home from './Containers/Home';
 import './App.css'
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            logged: true,
+            logged: false,
+            key: '',
         };
     }
 
     render() {
+        const {logged} = this.state;
+        const {mitter} = this.props;
         return (
             <Switch>
-                <Route exact path="/login" />
-                <Route path="/" />
-                <Redirect to="/login" />
+                <Route
+                    exact
+                    path="/auth"
+                    render={props => <Auth
+                        {...props}
+                        logged={logged}
+                    />}
+                />
+                <Route
+                    path="/"
+                    render={props => <Home
+                        {...props}
+                        logged={logged}
+                        mitter={mitter}
+                    />}
+                />
+                <Redirect to="/auth" />
             </Switch>
         );
     }
