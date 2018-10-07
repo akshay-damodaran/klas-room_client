52205565223a7e04c819a71126f44549a9f4aba8
Jagrutee Banda <jagruteebanda99@gmail.com>
2018-10-07 12:33:17 +0530

    Components & Containers

---
+++ src/Components/SideBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SideBar extends Component {
+    render() {
+        return (
+            <div className='SideBar'>
+                <h2 className='application-title'>
+                  My Chat App
+              </h2>
+          </div>
+        );
+    }
+}
+
+export default SideBar;
---
+++ src/Containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className='Home'>
+                <h2 className='application-title'>
+                  My Chat App
+              </h2>
+          </div>
+        );
+    }
+}
+
+export default Home;
