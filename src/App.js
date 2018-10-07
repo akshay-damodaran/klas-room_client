40aa4095aae182ff470b5ed9fdd634f7faa30888
akshay-damodaran <akshay.damodaran2411@gmail.com>
2018-10-07 14:11:12 +0530

    Commiting node modules

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
+import { Switch, Route, Redirect } from "react-router-dom";
 import './App.css'
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            logged: true,
+        };
+    }
+
     render() {
         return (
-            <div className='App'>
-                <h2 className='application-title'>
-                  My Chat App
-              </h2>
-          </div>
+            <Switch>
+                <Route exact path="/login" />
+                <Route path="/" />
+                <Redirect to="/login" />
+            </Switch>
         );
     }
 }
---
+++ src/Containers/Login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Login from '../Components/Login';
+
+class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            login: true,
+        };
+    }
+
+    renderLogin() {
+        return (
+            
+        );
+    }
+
+    renderRegister() {
+        return (
+            <div>
+                <h1></h1>
+            </div>
+        )
+    }
+}
