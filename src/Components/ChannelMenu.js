1494b84990b8654c75c151993d3f00dc7f1488f8
Jagrutee Banda <jagruteebanda99@gmail.com>
2018-10-08 16:58:49 +0530

    search and subscribed/unsubscribed channel list

--- src/Components/ChannelMenu.js
+++ src/Components/ChannelMenu.js
@@ -5,9 +5,14 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
 import ImageIcon from '@material-ui/icons/Image';
-import WorkIcon from '@material-ui/icons/Work';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+
+
+// import WorkIcon from '@material-ui/icons/Work';
 
 import axios from 'axios';
+import _ from 'lodash';
 
 class ChannelMenu extends Component {
     constructor(props) {
@@ -16,21 +21,26 @@ class ChannelMenu extends Component {
             subscribedChannels: [{
                 channelId: 3,
                 isChannelActive: false,
-                channelName: 'Suddu'
+                channelName: 'Suddu',
+                channelTeacher: 'Sudarshana Patil'
             }, {
                 channelId: 4,
                 isChannelActive: false,
-                channelName: 'JD'
+                channelName: 'JD',
+                channelTeacher: 'Jagrutee Banda'
             }],
             unsubscribedChannels: [{
                 channelId: 1,
                 isChannelActive: true,
-                channelName: 'Avinash'
+                channelName: 'Avinash',
+                channelTeacher: 'Avinash Maurya'
             }, {
                 channelId: 2,
                 isChannelActive: false,
-                channelName: 'Damu'
-            }]
+                channelName: 'Damu',
+                channelTeacher: 'Akshay Damodaran'
+            }],
+            searchedResults: false
         }
     }
 
@@ -50,6 +60,15 @@ class ChannelMenu extends Component {
         });
     }
 
+    handleClick(item) {
+        this.setState({ channelViewItem: item });
+    }
+
+    handleInputChange(event) {
+        const searchKeyword = event.target.value;
+
+    }
+
     render() {
         return (
             <div className="channel-menu">
@@ -57,43 +76,65 @@ class ChannelMenu extends Component {
                     <img src={require("../images/user-icon.svg")} className="img" />
                     <h3>{'Welcome Guest!'}</h3>
                 </div>
+                <div className="channel-menu search">
+                    <input
+                        className="channel-menu search-input"
+                        onChange={(e) => this.handleInputChange(e)}
+                        placeholder={'Search your channel'}
+                    />
+                </div>
                 <div className="channel-menu list">
-                    <div className="list search">
-                        <Input
-                            className="list search-input"
-                            onClick={() => { }}
-                            placeholder={'Search your channel'}
-                        />
-                    </div>
-                    <div className="list subscribed">
-                        <List>
-                            {
-                                this.state.subscribedChannels.map((item) =>
-                                    <ListItem>
-                                        <Avatar>
-                                            <ImageIcon />
-                                        </Avatar>
-                                        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-                                    </ListItem>
-                                )
-                            }
-                        </List>
-                    </div>
-                    <div className="list unsubscribed">
-                        <List>
-                            {
-                                this.state.unsubscribedChannels.map((item) =>
-                                    <ListItem>
-                                        <Avatar>
-                                            <ImageIcon />
-                                        </Avatar>
-                                        <h1>{item.channelName}</h1>
-                                        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-                                    </ListItem>
-                                )
-                            }
-                        </List>
-                    </div>
+                    {
+                        (this.state.searchedResults) ?
+                            <div className="channel-menu search-menu">
+                                <List>
+                                    {
+                                        (this.state.searchedResults).map((item) =>
+                                            <ListItem onClick={() => this.handleClick(item)}>
+                                                <Avatar>
+                                                    <ImageIcon />
+                                                </Avatar>
+                                                <ListItemText primary={item.channelName} secondary={"Taught by " + item.channelTeacher} color={'#'} />
+                                            </ListItem>
+                                        )
+                                    }
+                                </List>
+                            </div>
+                            :
+                            <div className="channel-menu list-menu">
+                                <div className="channel-menu subscribed">
+                                    <List>
+                                        {
+                                            this.state.subscribedChannels.map((item) =>
+                                                <ListItem onClick={() => this.handleClick(item)}>
+                                                    <Avatar>
+                                                        <ImageIcon />
+                                                    </Avatar>
+                                                    <ListItemText primary={item.channelName} secondary={"Taught by " + item.channelTeacher} color={'#'} />
+                                                </ListItem>
+                                            )
+                                        }
+                                    </List>
+                                </div>
+                                <div className="channel-menu unsubscribed">
+                                    <List>
+                                        {
+                                            this.state.unsubscribedChannels.map((item) =>
+                                                <ListItem>
+                                                    <Avatar>
+                                                        <ImageIcon />
+                                                    </Avatar>
+                                                    <ListItemText primary={item.channelName} secondary={"Taught by " + item.channelTeacher} />
+                                                    <Button variant="fab" mini color="secondary" aria-label="Add">
+                                                        <AddIcon />
+                                                    </Button>
+                                                </ListItem>
+                                            )
+                                        }
+                                    </List>
+                                </div>
+                            </div>
+                    }
                 </div>
             </div>
         );
