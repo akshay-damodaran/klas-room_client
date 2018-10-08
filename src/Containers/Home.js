fda94786cb1af38222e2de7d66631fdc405a9998
Jagrutee Banda <jagruteebanda99@gmail.com>
2018-10-08 06:15:17 +0530

    layout fixes to be done

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,44 +1,25 @@
 import React, { Component } from 'react';
 import ChannelMenu from '../Components/ChannelMenu';
 import ChannelView from '../Components/ChannelView';
+import '../styles/Home.css';
 
 class Home extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            channelMessages: [{
-                channelId: 1,
-                isChannelActive: true,
-                channelName: 'Avinash'
-            }, {
-                channelId: 2,
-                isChannelActive: false,
-                channelName: 'Damu'
-            }, {
-                channelId: 3,
-                isChannelActive: false,
-                channelName: 'Suddu'
-            }, {
-                channelId: 4,
-                isChannelActive: false,
-                channelName: 'JD'
-            }]
+
         }
     }
 
     render() {
         return (
-            <div className='Home'>
-                <h2 className='application-title'>
-                    Klas-room
-                </h2>
-                <ChannelMenu
-                />
-                <ChannelView
-                    mitter={this.props.mitter}
-                    channelMessages={this.state.channelMessages}
-                />
+            <div className="home">
+                <div className="home channel-menu">
+                    <ChannelMenu/>
+                </div>
+                <div className="home channel-view">
+                    <ChannelView/>
+                </div>
             </div>
         );
     }
